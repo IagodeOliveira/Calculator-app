d535bc3ed6911c7bfbba1e3bd0fbd11795c612e2
IagodeOliveira <iago.idom@gmail.com>
2021-07-25 23:23:51 -0300

    updated README.md and deleted others

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 
-
-//olhar o moz-
-//tentar fazer o metodo vma sei la
-// fazer o readMe
-
-
 // Global variables
-// const screen = document.querySelector('.screen');
 let previuos = document.querySelector('.previous');
 let current = document.querySelector('.current');
 const board = document.querySelector('.grid-container');
@@ -16,7 +9,6 @@ const toppper = document.querySelector('.top');
 let keys1 = [];
 let keys2 = [];
 let operators = [];
-// let screenView = [];
 let key = '';
 let outcome = [];
 let output = '';
@@ -233,22 +225,6 @@ board.addEventListener('click', (e) => {
     }
 });
 
-
-
-// Including commas if number is longer than 3 digits
-function hasComma(array) {
-    if(!array.includes('.') && !array.includes('-')) {
-        if(array.length > 3) {
-            return [Number(array.join('')).toLocaleString('en')];
-        }
-    }
-    if(array.includes('-')) {
-        if(array.length > 4) {
-            return [Number(array.join('')).toLocaleString('en')];
-        }
-    }
-}
-
 // Controls delete button
 function deletion() {
     const dotOperator = document.getElementsByClassName('dot')[0];
@@ -276,71 +252,6 @@ function reset() {
     const dotOperator = document.getElementsByClassName('dot')[0];
     dotOperator.classList.add("number1");
     dotOperator.classList.add("number2");
-}
-
-// Computes the math operations
-function result() {
-
-    let numCheck = keys2.findIndex(res => {
-        return typeof(res) == 'number';
-    });
-
-    if(numCheck != -1){
-        let dotCheck = keys2[keys2.length - 1];
-        if(dotCheck != '.') {
-            let num1 = Number(keys1.join(''));
-            let num2 = Number(keys2.join(''));
-            let operation = operators[0];
-
-            switch(operation) {
-                case '+':
-                    output = num1 + num2;
-                    outcome = resultFormat(output);
-                    break;
-                case '-':
-                    output = num1 - num2; 
-                    outcome = resultFormat(output);
-                    break;
-                case 'x':
-                    output = num1 * num2;
-                    outcome = resultFormat(output);
-                    break;
-                case '/':
-                    output = num1 / num2;
-                    if(num2 == 0) {
-                        reset();
-                        return
-                    }   
-                    outcome = resultFormat(output);
-                    break;
-                default:
-                    console.log('An error has ocorred');
-                    return
-            }
-            keys1 = [];
-            operators = [];
-            keys2 = [];
-        }
-    }
-}
-
-// Controls output's precision
-function resultFormat(output) {
-    if(output.toString().length > 12) {
-        if(output.toString().includes('e')) {
-            output = 0;
-            return [output];
-        }
-        if(output.toPrecision(3).includes('e')) {
-            return [output];
-        } else if(output > 1) {
-            return [output.toPrecision(3)];
-        } else {
-            return [output.toPrecision(2)];
-        }
-    } else {
-        return [output];
-    }
 }
 
 // Controlling the theme
---
+++ model.js
@@ -0,0 +1,78 @@
+// Including commas if number is longer than 3 digits
+function hasComma(array) {
+    if(!array.includes('.') && !array.includes('-')) {
+        if(array.length > 3) {
+            return [Number(array.join('')).toLocaleString('en')];
+        }
+    }
+    if(array.includes('-')) {
+        if(array.length > 4) {
+            return [Number(array.join('')).toLocaleString('en')];
+        }
+    }
+}
+
+// Computes the math operations
+function result() {
+
+    let numCheck = keys2.findIndex(res => {
+        return typeof(res) == 'number';
+    });
+
+    if(numCheck != -1){
+        let dotCheck = keys2[keys2.length - 1];
+        if(dotCheck != '.') {
+            let num1 = Number(keys1.join(''));
+            let num2 = Number(keys2.join(''));
+            let operation = operators[0];
+
+            switch(operation) {
+                case '+':
+                    output = num1 + num2;
+                    outcome = resultFormat(output);
+                    break;
+                case '-':
+                    output = num1 - num2; 
+                    outcome = resultFormat(output);
+                    break;
+                case 'x':
+                    output = num1 * num2;
+                    outcome = resultFormat(output);
+                    break;
+                case '/':
+                    output = num1 / num2;
+                    if(num2 == 0) {
+                        reset();
+                        return
+                    }   
+                    outcome = resultFormat(output);
+                    break;
+                default:
+                    console.log('An error has ocorred');
+                    return
+            }
+            keys1 = [];
+            operators = [];
+            keys2 = [];
+        }
+    }
+}
+
+// Controls output's precision
+function resultFormat(output) {
+    if(output.toString().length > 12) {
+        if(output.toString().includes('e')) {
+            output = 0;
+            return [output];
+        }
+        if(output.toPrecision(3).includes('e')) {
+            return [output];
+        } else if(output > 1) {
+            return [output.toPrecision(3)];
+        } else {
+            return [output.toPrecision(2)];
+        }
+    } else {
+        return [output];
+    }
+}
